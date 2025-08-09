5bb80857d51efc85198209543c6bb8cce6f6ba93
Keiji <teltel1991@gmail.com>
2025-08-09 20:55:20 +0900

    Update & Delete file

--- docker/vault/tests/vault_test.py
+++
@@ -1,9 +0,0 @@
-import hvac
-import os
-
-client = hvac.Client(url="https://127.0.0.1:8200", verify=False)
-
-role_id = os.environ["PROFINAUT_DB_ROLE_ID"]
-secret_id = os.environ["PROFINAUT_DB_SECRET_ID"]
-
-client.auth_approle(role_id, secret_id)
